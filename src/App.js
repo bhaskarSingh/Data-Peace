c933ba1a3cf219bfc7cb95b7faf96229f6869ce5
Bhaskar kumar <bhaskarkumar@Bhaskars-MacBook-Air.local>
2019-10-10 23:49:16 +0530

    fix: handle search input correctly

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Layout, Header, Pagination } from "./Components";
+import { Layout, Header, Pagination, Table } from "./Components";
 import axios from "axios";
-import styled from "@emotion/styled";
 function App() {
   const [users, updateUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [currentList, setCurrentList] = useState([]);
   const [searchInput, onInputChange] = useState("");
-  const [filterArr, setFilteredArr] = useState([]);
+  const [filteredLength, setFilteredLength] = useState(1);
+  const [filteredList, setFilteredList] = useState([]);
   const onPageChange = page => {
     setCurrentPage(page);
     const end = page * 5;
     const start = end - 5;
-    setCurrentList(users.slice(start, end));
+    const value =
+      searchInput === ""
+        ? users.slice(start, end)
+        : filteredList.slice(start, end);
+    setCurrentList(value);
   };
   const ApiRequest = async () => {
     const result = await axios("https://demo9197058.mockable.io/users");
@@ -23,45 +27,6 @@ function App() {
   useEffect(() => {
     ApiRequest();
   }, []);
-
-  const columns = [
-    {
-      label: "First Name",
-      path: "first_name"
-    },
-    {
-      label: "Last Name",
-      path: "last_name"
-    },
-    {
-      label: "Company Name",
-      path: "company_name"
-    },
-    {
-      label: "City",
-      path: "city"
-    },
-    {
-      label: "State",
-      path: "state"
-    },
-    {
-      label: "Zip",
-      path: "zip"
-    },
-    {
-      label: "Email",
-      path: "email"
-    },
-    {
-      label: "Web",
-      path: "web"
-    },
-    {
-      label: "Age",
-      path: "age"
-    }
-  ];
 
   return (
     <Layout>
@@ -75,15 +40,18 @@ function App() {
             value={searchInput}
             onChange={e => {
               if (e.target.value === "") {
-                setCurrentList(users.slice(0, 5));
+                const end = currentPage * 5;
+                const start = end - 5;
+                setCurrentList(users.slice(start, end));
               } else {
                 const result = users.filter(user =>
                   user.first_name.toLowerCase().includes(e.target.value)
                 );
-                setFilteredArr(result);
                 const end = currentPage * 5;
                 const start = end - 5;
                 setCurrentList(result.slice(start, end));
+                setFilteredLength(result.length);
+                setFilteredList(result);
                 setCurrentPage(1);
               }
               onInputChange(e.target.value);
@@ -91,28 +59,9 @@ function App() {
           />
         </label>
       </form>
-      <Table>
-        <thead>
-          <tr>
-            {columns.map(column => {
-              return <th key={column.path}>{column.label}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {currentList.map(list => {
-            return (
-              <tr key={list.id}>
-                {columns.map((column, index) => {
-                  return <td key={index}>{list[column.path]}</td>;
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      <Table currentList={currentList} />
       <Pagination
-        itemsCount={searchInput === "" ? users.length : filterArr.length}
+        itemsCount={searchInput === "" ? users.length : filteredLength}
         pageSize={5}
         currentPage={currentPage}
         onPageChange={onPageChange}
@@ -120,30 +69,5 @@ function App() {
     </Layout>
   );
 }
-
-const Table = styled.table`
-  th,
-  tr {
-    border: 1px solid #ddd;
-    padding: 20px;
-  }
-
-  th {
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: left;
-    background-color: #007bff;
-    color: white;
-  }
-
-  tr:nth-child(even) {
-    background-color: #f2f2f2;
-  }
-
-  tr:hover {
-    background-color: #ddd;
-    cursor: pointer;
-  }
-`;
 
 export default App;
