67caa746abb6624fe3fb40b576d39bedcdcaadaf
Bhaskar kumar <bhaskarkumar@Bhaskars-MacBook-Air.local>
2019-10-13 23:49:11 +0530

    test: add for Layout component

--- src/Components/Layout/Layout.test.js
+++ src/Components/Layout/Layout.test.js
@@ -2,4 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Layout from "./Layout";
 
-test("render child node");
+test("render child node", () => {
+  const childComponent = <main>yo</main>;
+  const { getByText } = render(<Layout>{childComponent}</Layout>);
+  expect(getByText("yo").textContent).toBe("yo");
+});
